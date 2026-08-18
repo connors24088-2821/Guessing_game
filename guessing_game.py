39a4532ae2c61c53196caead328a80c6e054e4cc
connors24088-2821 <connors.24088@obhs.school.nz>
2026-08-19 09:32:48 +1200

    list_maker
    I made a random list maker to get a random number from 1 to 100

--- guessing_game.py
+++ guessing_game.py
@@ -7,19 +7,27 @@
 '''
 
 #------------librarirs----------------------
-
+import random
 
 #------------functions----------------------
-
+def list_maker():
+    """ This function create a list of numbers from 1-100 and stores it in a list. @p -none. @r -return type list of ints"""
+    rand_numbers = []
+    # create a list of numbers
+    for i in range(1,101):
+        rand_numbers.append(i)
+    return rand_numbers
 
 #------------main routine----------------------
-
+list_numbers = []
 if(__name__=="__main__"):
     # enter a name
     # enter age
     # make a list of number
+    list_maker()# it will make a list of number bettween 1 and 100
     # intro to the game
     #computer rand number
+    rand_nu, = random.choice()
     # ask user to guess 
     # give higher or lower
     # count amount of guesses
