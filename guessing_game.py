4f0b3d02f14c69547b3cf5d8cce6895fc68cae64
connors24088-2821 <connors.24088@obhs.school.nz>
2026-08-19 09:11:35 +1200

    sectioning off
    I have started by sectioning off my code

---
+++ guessing_game.py
@@ -0,0 +1,18 @@
+'''
+    author: Connor Sinclair
+    date: 19/03/26
+    version: 1
+    description: This is a guessing game where you guess a number bettween 1 t0 100
+
+
+'''
+
+
+#------------librarirs----------------------
+
+
+#------------functions----------------------
+
+
+
+#------------main routine----------------------
